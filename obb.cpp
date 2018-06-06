ef0b874b5532e2d924fb6177b7e78a14c8061aea
cadangelo <cadangelo@wisc.edu>
2018-06-06 09:14:34 -0500

    adding/deleting vol and surf trees

--- obb.cpp
+++ obb.cpp
@@ -90,10 +90,21 @@ MB_CHK_SET_ERR(rval, "Error creating meshset.");
 rval = MBI->load_file(filename.c_str(), &fileset);
 MB_CHK_SET_ERR(rval, "Error loading file.");
 
-//Build obb trees
 GTT = new GeomTopoTool(MBI);
+ 
+//Set up IC
+rval = GTT->setup_implicit_complement();
+MB_CHK_SET_ERR(rval, "Error creating IC.");
+EntityHandle ic_vol;
+rval = GTT->get_implicit_complement(ic_vol);
+MB_CHK_SET_ERR(rval, "Error getting IC.");
+
+//Build obb trees
 rval = GTT->construct_obb_trees(false);
+//rval = GTT->construct_obb_tree(ic_vol);
 MB_CHK_SET_ERR(rval, "Error constructing all trees.");
+std::cout << "num created trees " << GTT->obb_tree()->num_created_trees() << std::endl;
+
 //EntityHandle modelSet;
 //modelSet = GTT->get_root_model_set();
 
@@ -114,10 +125,31 @@ MB_CHK_SET_ERR(rval, "Error getting 3 ents.");
 std::cout << "num vols in model set " << num_vols << std::endl;
 
 //Get root of a volume obb
-EntityHandle root, sroot;
-rval = GTT->get_root(*vols.begin(), root);
+EntityHandle icroot;
+rval = GTT->get_root(ic_vol, icroot);
+MB_CHK_SET_ERR(rval, "Error getting IC root.");
+std::cout << "ic root eh " << icroot << std::endl;
+
+// build first vol tree and get root
+EntityHandle first_vol = *vols.begin();
+std::cout << "first vol " << first_vol << std::endl;
+EntityHandle first_surf = *surfs1.begin();
+std::cout << "first surf " << first_surf << std::endl;
+
+//rval = GTT->construct_obb_tree(first_surf);
+//MB_CHK_SET_ERR(rval, "Error building surf tree.");
+
+//rval = GTT->construct_obb_tree(first_vol);
+//MB_CHK_SET_ERR(rval, "Error building vol tree.");
+
+
+EntityHandle root, sroot, first_vol_root, first_surf_root;
+rval = GTT->get_root(first_vol, first_vol_root);
 MB_CHK_SET_ERR(rval, "Error getting root.");
-std::cout << "root eh " << root << std::endl;
+rval = GTT->get_root(first_surf, first_surf_root);
+MB_CHK_SET_ERR(rval, "Error getting root.");
+std::cout << "first vol root eh " << first_vol_root << std::endl;
+std::cout << "surf root eh " << first_surf_root << std::endl;
 
 // Delete surface trees and root
 //Range surfs;
@@ -134,16 +166,30 @@ std::cout << "root eh " << root << std::endl;
 //
 //}
 
-// Unjoin vol tree
-rval = GTT->obb_tree()->unjoin_trees(root);
+// Unjoin IC vol tree-- this seems to work, set aside for now
+rval = GTT->obb_tree()->unjoin_trees(icroot);
 MB_CHK_SET_ERR(rval, "Error unjoining vol tree.");
+
+// Delete surf obb tree
+rval = GTT->obb_tree()->delete_tree_not_ents(first_surf_root);
+MB_CHK_SET_ERR(rval, "Error removing surf tree.");
+rval = GTT->construct_obb_tree(first_surf);
+MB_CHK_SET_ERR(rval, "Error building surf tree.");
   
-// Delete old vol obb tree
-//rval = GTT->obb_tree()->delete_tree(*vols.begin());
-//MB_CHK_SET_ERR(rval, "Error removing surf tree.");
+// Delete a vol obb tree
+rval = GTT->is_owned_set(first_vol);
+MB_CHK_SET_ERR(rval, "vol not in model set.");
+
+rval = GTT->obb_tree()->delete_tree_not_ents(first_vol_root);
+MB_CHK_SET_ERR(rval, "Error removing vol tree.");
+
+rval = GTT->is_owned_set(first_vol);
+MB_CHK_SET_ERR(rval, "2: vol not in model set.");
+
+// Rebuild vol tree
+
+
 //
-//rval = GTT->get_gsets_by_dimension(2, surfs);
-//std::cout << "num surfs gsets " << surfs.size() << std::endl;
 //rval = GTT->get_root(*vols.begin(), sroot);
 //MB_CHK_SET_ERR(rval, "Error finding vol root.");
 //std::cout << "orig vol root eh " << sroot << std::endl;
@@ -164,11 +210,15 @@ MB_CHK_SET_ERR(rval, "Error unjoining vol tree.");
 
 
 
-//rval = GTT->construct_obb_tree(*vols.begin());
-//MB_CHK_SET_ERR(rval, "Error building vol tree.");
+rval = GTT->construct_obb_tree(first_vol);
+MB_CHK_SET_ERR(rval, "Error building vol tree.");
+
+rval = GTT->get_root(first_vol, first_vol_root);
+MB_CHK_SET_ERR(rval, "Error getting root.");
 //  rval = GTT->get_root(*vols.begin(), sroot);
 //  MB_CHK_SET_ERR(rval, "Error finding vol root.");
-//std::cout << "new vol root eh " << sroot << std::endl;
+std::cout << "new vol root eh " << first_vol_root << std::endl;
+std::cout << "num created trees " << GTT->obb_tree()->num_created_trees() << std::endl;
 
 
 
